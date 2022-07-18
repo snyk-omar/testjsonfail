903466935e524871450a013cfa3863a86951fe5a
mike <michaelbraunbass@gmail.com>
2022-07-18 15:21:32 -0600

    update

---
+++ tfcloud_goof/vuln_ubuntu.tf
@@ -0,0 +1,31 @@
+#Variable Processing
+# Setup the userdata that will be used for the instance
+data "template_file" "userdata_setup" {
+  template = file("userdata_setup.template")
+
+  vars  = {
+    logic = file("vuln_bootstrap.sh")
+  }
+}
+
+
+resource "aws_instance" "vuln_vm" {
+  availability_zone = var.primary_az
+  ami           = var.ubuntu_ami
+  instance_type = "t2.micro"
+  key_name = var.key_name
+  network_interface {
+        device_index = 0
+        network_interface_id = aws_network_interface.goof-nic.id
+  }
+  
+  user_data = data.template_file.userdata_setup.rendered
+  tags = {
+    Environment = "dev"
+  }
+}
+
+output "public_ip" {
+  description = "List of public IP addresses assigned to the instances, if applicable"
+  value       = aws_instance.vuln_vm.public_ip
+}
